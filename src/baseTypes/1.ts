840186dbf63e8ee2862d3211ecc1cb80687fc151
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-12-05 19:04:32 +0200

    file

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,9 +1,9 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
 let age = 50;
-let name = 'Max';
+let name = 'Maxs';
 let toggle = true;
-let empty = nullg;
+let empty = null;
 let notInitialize;
 let callback = (a) => { return 100 + a };
 
